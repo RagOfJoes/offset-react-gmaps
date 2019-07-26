a6f70558552e35f6cbd7139728d6ee98b13edd08
Victor Ragojos <vhsvragojos@gmail.com>
2019-07-26 16:43:58 -0700

    added additional fields to coords | created regions obj

--- src/config/coords.js
+++ src/config/coords.js
@@ -1,67 +1,84 @@
-export const coordinates = [
-    {
-        Vineyard: "Last Stop",
+export const coordinates = {
+    "Last Stop": {
         lat: 38.470977,
-        lng: -122.89356
+        lng: -122.89356,
+        location: "Russian River Valley, California",
+        image: require("../assets/temp-vineyard-img.png")
     },
-    {
-        Vineyard: "West Pin",
+    "West Pin": {
         lat: 38.5211001,
-        lng: -122.8662024
+        lng: -122.8662024,
+        location: "Russian River Valley, California",
+        image: require("../assets/temp-vineyard-img.png")
     },
-    {
-        Vineyard: "Garys",
+    "Garys'": {
         lat: 36.4422663,
-        lng: -121.4402578
+        lng: -121.4402578,
+        location: "Santa Lucia Highlands, California",
+        image: require("../assets/temp-vineyard-img.png")
     },
-    {
-        Vineyard: "Soberanes",
+    "Soberanes": {
         lat: 36.4407934,
-        lng: -121.4391096
+        lng: -121.4391096,
+        location: "Santa Lucia Highlands, California",
+        image: require("../assets/temp-vineyard-img.png")
     },
-    {
-        Vineyard: "Bien Nacido",
+    "Bien Nacido": {
         lat: 34.8946711,
-        lng: -120.298725
+        lng: -120.298725,
+        location: "Santa Maria Valley, California",
+        image: require("../assets/temp-vineyard-img.png")
     },
-    {
-        Vineyard: "Prince Hill",
+    "Prince Hill": {
         lat: 45.2825284,
-        lng: -123.0408265
+        lng: -123.0408265,
+        location: "Dundee Hills, Oregon",
+        image: require("../assets/temp-vineyard-img.png")
     },
-    {
-        Vineyard: "Healdsburg Estate",
+    "Healdsburg Estate": {
         lat: 38.5781274,
-        lng: -122.8758549
+        lng: -122.8758549,
+        location: "Russian River Valley, California",
+        image: require("../assets/temp-vineyard-img.png")
     },
-    {
-        Vineyard: "Merino",
+    "Merino": {
         lat: 38.4365445,
-        lng: -122.7832404
+        lng: -122.7832404,
+        location: "Russian River Valley, California",
+        image: require("../assets/temp-vineyard-img.png")
     },
-    {
-        Vineyard: "Oakville Estate",
+    "Oakville Estate": {
         lat: 38.4411547,
-        lng: -122.3828042
+        lng: -122.3828042,
+        location: "Napa Valley, California",
+        image: require("../assets/temp-vineyard-img.png")
     },
-    {
-        Vineyard: "Soda Canyon Ranch",
+    "Soda Canyon Ranch": {
         lat: 38.3578970,
-        lng: -122.278065
+        lng: -122.278065,
+        location: "Napa Valley, California",
+        image: require("../assets/temp-vineyard-img.png")
     },
-    {
-        Vineyard: "Calistoga Estate",
+    "Calistoga Estate": {
         lat: 38.5658304,
-        lng: -122.5561176
+        lng: -122.5561176,
+        location: "Napa Valley, California",
+        image: require("../assets/temp-vineyard-img.png")
     },
-    {
-        Vineyard: "Monument Tree",
+    "Monument Tree": {
         lat: 39.1149364,
-        lng: -123.5132721
+        lng: -123.5132721,
+        location: "Anderson Valley, California",
+        image: require("../assets/temp-vineyard-img.png")
     },
-    {
-        Vineyard: "Philo Estate",
+    "Philo Estate": {
         lat: 39.1097857,
-        lng: -123.4917431
+        lng: -123.4917431,
+        location: "Anderson Valley, California",
+        image: require("../assets/temp-vineyard-img.png")
     }
-]
+}
+
+export const vineyardNames = ["Prince Hill", "Monument Tree", "Philo Estate", "Healdsburg Estate",
+    "West Pin", "Last Stop", "Merino", "Calistoga Estate", "Oakville Estate", "Soda Canyon Ranch",
+    "Garys'", "Soberanes", "Bien Nacido"]
---
+++ src/config/regions.js
@@ -0,0 +1,19 @@
+export const sections = [
+    {
+        sectionTitle: "Oregon",
+        appellations: ["Willamette Valley", "Dundee Hills"],
+        caption: "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit sed.",
+        vineyardNames: ["Prince Hill"]
+    },
+    {
+        sectionTitle: "North Coast",
+        appellations: ["Anderson Valley", "Russian River Valley", "Dry Creek Valley", "Napa Valley", "Oakville", "Calistoga"],
+        vineyardNames: ["Monument Tree", "Philo Estate", "Healdsburg Estate", "West Pin", "Last Stop", "Merino", 
+        "Calistoga Estate", "Oakville Estate", "Soda Canyon Ranch"]
+    },
+    {
+        sectionTitle: "Central Coast",
+        appellations: ["Santa Maria Valley", "Santa Lucia Highlands"],
+        vineyardNames:["Garys'", "Soberanes", "Bien Nacido"]
+    }
+]
