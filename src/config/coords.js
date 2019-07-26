a9ca75b08d01c345d77417aa06107d2d1046abd2
Victor Ragojos <victorragojos@Victors-MacBook.local>
2019-07-25 17:18:43 -0700

    created coords config Array(For dynamic content)

---
+++ src/config/coords.js
@@ -0,0 +1,67 @@
+export const coordinates = [
+    {
+        Vineyard: "Last Stop",
+        lat: 38.470977,
+        lng: -122.89356
+    },
+    {
+        Vineyard: "West Pin",
+        lat: 38.5211001,
+        lng: -122.8662024
+    },
+    {
+        Vineyard: "Garys",
+        lat: 36.4422663,
+        lng: -121.4402578
+    },
+    {
+        Vineyard: "Soberanes",
+        lat: 36.4407934,
+        lng: -121.4391096
+    },
+    {
+        Vineyard: "Bien Nacido",
+        lat: 34.8946711,
+        lng: -120.298725
+    },
+    {
+        Vineyard: "Prince Hill",
+        lat: 45.2825284,
+        lng: -123.0408265
+    },
+    {
+        Vineyard: "Healdsburg Estate",
+        lat: 38.5781274,
+        lng: -122.8758549
+    },
+    {
+        Vineyard: "Merino",
+        lat: 38.4365445,
+        lng: -122.7832404
+    },
+    {
+        Vineyard: "Oakville Estate",
+        lat: 38.4411547,
+        lng: -122.3828042
+    },
+    {
+        Vineyard: "Soda Canyon Ranch",
+        lat: 38.3578970,
+        lng: -122.278065
+    },
+    {
+        Vineyard: "Calistoga Estate",
+        lat: 38.5658304,
+        lng: -122.5561176
+    },
+    {
+        Vineyard: "Monument Tree",
+        lat: 39.1149364,
+        lng: -123.5132721
+    },
+    {
+        Vineyard: "Philo Estate",
+        lat: 39.1097857,
+        lng: -123.4917431
+    }
+]
