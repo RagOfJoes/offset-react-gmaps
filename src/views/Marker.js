90e519cb8276182e72326635cd95e2f4adb2932c
Victor Ragojos <victorragojos@Victors-MacBook.local>
2019-07-25 17:18:22 -0700

    added custom Marker icons

--- src/views/Marker.js
+++ src/views/Marker.js
@@ -6,12 +6,21 @@ import { useDispatch, useSelector } from 'react-redux';
 
 const CustomMarker = (props) => {
     const dispatch = useDispatch();
+    const refs = {
+        marker: undefined
+    }
     const { mapRef } = useSelector(state => state.Map)
     const { lat, lng, text, isFocused } = props;
     return (
         <Marker
             clickable
             key={text}
+            icon= {
+                {
+                    url: require('../assets/CustomMarker.svg')
+                }
+            }
+            ref={ref => refs.marker = ref}
             position={{ lat, lng }}
             opacity={isFocused ? 1 : .7}
             onClick={(e) => {
