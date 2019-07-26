9bd4f4424ee37bee90708faa8ae8cf68335a9517
RagOfJoes <vhsvragojos@gmail.com>
2019-07-25 22:16:48 -0700

    created a bit more generic code for later uses

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import './App.scss';
 import React from 'react';
 import Map from './views/Map';
 import Marker from './views/Marker';
-import { Container, Row, Col } from 'reactstrap';
-import { coordinates } from './config/coords';
 import { useSelector } from 'react-redux';
+import { mapStyle } from './config/mapStyle';
+import { coordinates } from './config/coords';
+import { Container, Row, Col } from 'reactstrap';
 
 // TODO: Get API key
 // TODO: Import google maps API
@@ -12,25 +13,66 @@ const App = () => {
   // console.log(process.env)
   const { mapCenter } = useSelector((state) => state.Map);
   return (
+    // TODO: Create Card component and 
     <Container fluid className="App">
       <Row className="scroll-places-row">
         <Col>
           <h1>NORTHERN CALIFORNIA</h1>
         </Col>
       </Row>
+
+      {/* Start Map View */}
       <Map
-        defaultCenter={{ lat:38.5781274, lng: -122.8758549}}
+        // Map style props
+        defaultZoom={10}
+        mapClassName="scroll-map-row"
+        containerClassName="scroll-map-container"
+
+        // Required props
+        apiKey="AIzaSyAfLyiMPaR2VvvyGTqY7S6kX-SYcjUJyYE"
+        defaultCenter={{ lat: 38.5781274, lng: -122.8758549 }}
+
+        // KML Layer Props
+        hasKmlLayer
+        kmlLayerURL="http://www.google.com/maps/d/kml?forcekml=1&mid=1GzhhLKvqqJfFwnxdnkwW5q8qVaWZpzPI"
+
+        // Map Options Props
+        mapOptions={
+          {
+            minZoom: 10,
+            maxZoom: 13,
+            disableDefaultUI: true,
+
+            // Custom Map Style
+            styles: mapStyle
+          }
+        }
       >
         {
+          // Map entire coordinates array for Markers and Vineyard names
           coordinates.map((coord) => {
             const { lat, lng, Vineyard } = coord;
             return (
-              <Marker key={Vineyard} lat={lat} lng={lng} isFocused={lat.toPrecision(10) === mapCenter.lat.toPrecision(10) && lng.toPrecision(10) === mapCenter.lng.toPrecision(10)} />
+              <Marker
+                // Position props
+                lat={lat}
+                lng={lng}
+
+                // Key/Ref props
+                key={Vineyard}
+
+                // customIcon prop
+                customIconImage={require("./assets/CustomMarker.svg")}
+
+                // isFocused props
+                isClickable
+                isFocused={lat.toPrecision(10) === mapCenter.lat.toPrecision(10) && lng.toPrecision(10) === mapCenter.lng.toPrecision(10)}
+              />
             )
           })
         }
-        {/* Markers go here */}
       </Map>
+      {/* End Map View */}
     </Container>
   );
 }
