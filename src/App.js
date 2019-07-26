df79ade69da41ba4555205cf0dac0d5174a45f95
Victor Ragojos <victorragojos@Victors-MacBook.local>
2019-07-25 17:18:56 -0700

    small prototype of App Layour

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
+import './App.scss';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Map from './views/Map';
+import Marker from './views/Marker';
+import { Container, Row, Col } from 'reactstrap';
+import { coordinates } from './config/coords';
+import { useSelector } from 'react-redux';
 
-function App() {
+// TODO: Get API key
+// TODO: Import google maps API
+const App = () => {
+  // console.log(process.env)
+  const { mapCenter } = useSelector((state) => state.Map);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid className="App">
+      <Row className="scroll-places-row">
+        <Col>
+          <h1>NORTHERN CALIFORNIA</h1>
+        </Col>
+      </Row>
+      <Map
+        defaultCenter={{ lat:38.5781274, lng: -122.8758549}}
+      >
+        {
+          coordinates.map((coord) => {
+            const { lat, lng, Vineyard } = coord;
+            return (
+              <Marker key={Vineyard} lat={lat} lng={lng} isFocused={lat.toPrecision(10) === mapCenter.lat.toPrecision(10) && lng.toPrecision(10) === mapCenter.lng.toPrecision(10)} />
+            )
+          })
+        }
+        {/* Markers go here */}
+      </Map>
+    </Container>
   );
 }
 
