3538cc79881bd8743c70419d56f42f15158fb5b5
RagOfJoes <vhsvragojos@gmail.com>
2019-07-28 03:50:24 -0700

    added comments as well as converted appellations to String instead of array for performance

--- src/config/regions.js
+++ src/config/regions.js
@@ -1,19 +1,40 @@
+/**
+ * @variable {Array} sections Array of different regions, this will be used to render @see See [src/views] Section.js
+ * @property {string} sectionTitle Title of the Section
+ * @property {string} appellations Region's appellations
+ * @property {string} caption Section's caption/description
+ * @property {string[]} vineyardNames Section's Vineyards, this will be used to render list of @see See [src/view] Card.js
+ * 
+ * @author [Victor Ragojos](https://github.com/RagOfJoes)
+ */
+
 export const sections = [
     {
         sectionTitle: "Oregon",
-        appellations: ["Willamette Valley", "Dundee Hills"],
-        caption: "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit sed.",
+        appellations: "Willamette Valley, Dundee Hills",
+        caption:
+            "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit sed.",
         vineyardNames: ["Prince Hill"]
     },
     {
         sectionTitle: "North Coast",
-        appellations: ["Anderson Valley", "Russian River Valley", "Dry Creek Valley", "Napa Valley", "Oakville", "Calistoga"],
-        vineyardNames: ["Monument Tree", "Philo Estate", "Healdsburg Estate", "West Pin", "Last Stop", "Merino", 
-        "Calistoga Estate", "Oakville Estate", "Soda Canyon Ranch"]
+        appellations:
+            "Anderson Valley, Russian River Valley, Dry Creek Valley, Napa Valley, Oakville, Calistoga",
+        vineyardNames: [
+            "Monument Tree",
+            "Philo Estate",
+            "Healdsburg Estate",
+            "West Pin",
+            "Last Stop",
+            "Merino",
+            "Calistoga Estate",
+            "Oakville Estate",
+            "Soda Canyon Ranch"
+        ]
     },
     {
         sectionTitle: "Central Coast",
-        appellations: ["Santa Maria Valley", "Santa Lucia Highlands"],
-        vineyardNames:["Garys'", "Soberanes", "Bien Nacido"]
+        appellations: "Santa Maria Valley, Santa Lucia Highlands",
+        vineyardNames: ["Garys'", "Soberanes", "Bien Nacido"]
     }
-]
+];
