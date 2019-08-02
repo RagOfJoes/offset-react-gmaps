7d1cc1a4c5b2ca5ad3e0b352f4f34f4b3d34b9fa
Victor Ragojos <vhsvragojos@gmail.com>
2019-08-02 13:01:04 -0700

    new infoWindow logic | Still WIP

--- src/views/Marker.js
+++ src/views/Marker.js
@@ -15,8 +15,6 @@ import { Marker, InfoWindow } from "react-google-maps";
  *   @see See [Wikipedia](https://tomchentw.github.io/react-google-maps/) for a list of different props
  */
 const CustomMarker = React.memo(props => {
-    const [isInfoWindowOpen, toggleInfoWindow] = useState(false);
-
     // Retrieve props
     const {
         lat,
@@ -29,61 +27,71 @@ const CustomMarker = React.memo(props => {
         hasInfoWindow,
         changeMapCenter,
         hasScrollEffect,
+        hasInfoWindowOpen,
+        toggleInfoWindow,
         infoWindowComponent
     } = props;
 
+    const [infoWindow, openInfoWindow] = useState(false);
+
+    const closeInfoWindow = () => {
+        toggleInfoWindow();
+        openInfoWindow(false);
+    };
+
     // Retrieve MapRef DOM Node from Redux
     return (
-        <div>
-            <Marker
-                key={text}
-                icon={{
-                    url: isFocused ? images.on : images.off
-                }}
-                animation={2}
-                clickable={isClickable}
-                position={{ lat, lng }}
-                zIndex={isFocused ? 2 : 1}
-                // Check if clicked marker is the same as mapCenter
-                // opacity={isClickable && isFocused ? 1 : 0.6}
-                opacity={
-                    hasInfoWindow
-                        ? isInfoWindowOpen
-                            ? 0
-                            : 1
-                        : isClickable && isFocused
-                        ? 1
-                        : .6
-                }
-                onClick={mouseEvent => {
-                    // Pan to given Lat and Long coord if mapMarker is clickable
-                    if (isClickable) {
-                        // Retrieve Lat and Long from Google Maps API event
-                        const lat = mouseEvent.latLng.lat();
-                        const lng = mouseEvent.latLng.lng();
+        <Marker
+            key={text}
+            icon={{
+                url: isFocused ? images.on : images.off
+            }}
+            // animation={2}
+            clickable={isClickable}
+            position={{ lat, lng }}
+            zIndex={isFocused ? 2 : 1}
+            // Check if clicked marker is the same as mapCenter
+            // opacity={isClickable && isFocused ? 1 : 0.6}
+            opacity={
+                hasInfoWindow
+                    ? infoWindow
+                        ? 0
+                        : 1
+                    : isClickable && isFocused
+                    ? 1
+                    : 0.6
+            }
+            onClick={mouseEvent => {
+                // Pan to given Lat and Long coord if mapMarker is clickable
+                if (isClickable) {
+                    // Retrieve Lat and Long from Google Maps API event
+                    const lat = mouseEvent.latLng.lat();
+                    const lng = mouseEvent.latLng.lng();
 
-                        // Scrolls to respective Card Component
+                    // Toggles Info Window
+                    if (hasInfoWindow && !hasInfoWindowOpen && !infoWindow) {
                         toggleInfoWindow(true);
-
-                        if (hasScrollEffect) {
-                            mapRef.panTo({ lat, lng });
-
-                            // Change Map Center for checking if marker isFocused
-                            changeMapCenter(lat, lng);
-                            document
-                                .getElementsByClassName(`${text}`)[0]
-                                .scrollIntoView();
-                        }
+                        openInfoWindow(true);
                     }
-                }}
-            >
-                {hasInfoWindow && isInfoWindowOpen ? (
-                    <InfoWindow onCloseClick={() => toggleInfoWindow(false)}>
-                        {infoWindowComponent}
-                    </InfoWindow>
-                ) : null}
-            </Marker>
-        </div>
+
+                    if (hasScrollEffect) {
+                        mapRef.panTo({ lat, lng });
+
+                        // Change Map Center for checking if marker isFocused
+                        changeMapCenter(lat, lng);
+                        document
+                            .getElementsByClassName(`${text}`)[0]
+                            .scrollIntoView();
+                    }
+                }
+            }}
+        >
+            {hasInfoWindow && infoWindow ? (
+                <InfoWindow onCloseClick={() => closeInfoWindow()}>
+                    {infoWindowComponent}
+                </InfoWindow>
+            ) : null}
+        </Marker>
     );
 });
 
