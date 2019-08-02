b4a87f0919f3d07a714f3b1f51403c0d8553bd0f
Victor Ragojos <vhsvragojos@gmail.com>
2019-08-02 10:14:59 -0700

    created different components for each region

---
+++ src/Containers/central-coast.js
@@ -0,0 +1,162 @@
+import React from "react";
+import Map from "../views/Map";
+import Card from "../views/Card";
+import Marker from "../views/Marker";
+import Section from "../views/Section";
+import { connect } from "react-redux";
+import { Container } from "reactstrap";
+import { mapStyle } from "../config/mapStyle";
+import { sections } from "../config/regions";
+import { coordinates } from "../config/coords";
+import {
+    assignScrollRef,
+    isScrolling,
+    assignMapRef,
+    changeMapCenter
+} from "../Redux/Actions/Map";
+
+class Region extends React.PureComponent {
+    componentDidMount() {
+        const { vineyardNames } = sections[2];
+        this.props.dispatch(
+            changeMapCenter(
+                coordinates[vineyardNames[0]].lat,
+                coordinates[vineyardNames[0]].lng
+            )
+        );
+    }
+
+    render() {
+        const { refs } = this.props;
+        const { center } = refs.map;
+
+        const { sectionTitle, appellations, vineyardNames } = sections[2];
+
+        const assignScroll = ref => {
+            // Prevents from executing assignRef multiple times
+            if (!this.scrollRef) {
+                this.scrollRef = ref;
+                this.props.dispatch(assignScrollRef(ref));
+            }
+        };
+
+        return (
+            <Container fluid className="App">
+                <div
+                    className="scroll-places-row"
+                    ref={ref => assignScroll(ref)}
+                    onScroll={() => this.props.dispatch(isScrolling(true))}
+                >
+                    {
+                        <Section
+                            key={sectionTitle}
+                            title={sectionTitle}
+                            appellations={appellations}
+                        >
+                            {vineyardNames.map(vineyard => {
+                                const {
+                                    lat,
+                                    lng,
+                                    caption,
+                                    location
+                                } = coordinates[vineyard]; // Get vineyard's info
+                                return (
+                                    <Card
+                                        key={vineyard}
+                                        title={vineyard}
+                                        caption={caption}
+                                        location={location}
+                                        position={{ lat, lng }}
+                                        mapElem={refs.map.ref}
+                                        scrollElem={this.scrollRef}
+                                    />
+                                );
+                            })}
+                        </Section>
+                    }
+                </div>
+                <Map
+                    // Map style props
+                    defaultZoom={10}
+                    mapClassName="scroll-map-row"
+                    containerClassName="scroll-map-container"
+                    // Required props
+                    mapRef={refs.map.ref}
+                    apiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY}
+                    defaultCenter={{
+                        lat: coordinates[vineyardNames[0]].lat,
+                        lng: coordinates[vineyardNames[0]].lng
+                    }}
+                    dispatchMapRef={ref =>
+                        this.props.dispatch(assignMapRef(ref))
+                    }
+                    // KML Layer Props, This is for the geofencing of Vineyards etc.
+                    hasKmlLayer
+                    kmlLayerURL="http://www.google.com/maps/d/kml?forcekml=1&mid=1GzhhLKvqqJfFwnxdnkwW5q8qVaWZpzPI"
+                    // Map Options Props
+                    mapOptions={{
+                        // Zoom control options
+                        // minZoom: 10,
+                        // maxZoom: 11,
+                        zoomControl: true, // shows Zoom buttons in Google Maps
+
+                        // UI Control
+                        disableDefaultUI: true,
+
+                        // Custom Map Style
+                        styles: mapStyle
+                    }}
+                >
+                    {// Map entire coordinates array for Markers and Vineyard names
+                    vineyardNames.map(vineyard => {
+                        const { lat, lng } = coordinates[vineyard];
+                        return (
+                            <Marker
+                                // Position props
+                                lat={lat}
+                                lng={lng}
+                                // Key/Ref props
+                                key={vineyard}
+                                text={vineyard}
+                                mapRef={refs.map.ref}
+                                // Actions dispatches
+                                changeMapCenter={(lat, lng) =>
+                                    this.props.dispatch(
+                                        changeMapCenter(lat, lng)
+                                    )
+                                }
+                                // ScrollEffect
+                                hasScrollEffect
+                                // customIcon prop
+                                images={{
+                                    on: require("../assets/markeron.svg"),
+                                    off: require("../assets/markeroff.svg")
+                                }}
+                                // InfoWindow props
+                                hasInfoWindow={false}
+                                // isFocused props
+                                isClickable
+                                /**
+                                 * toPrecision method is for ensuring that lang & lng returned by onClick event, @see See [src/views](Marker), are the same as,
+                                 * @see See [src/config](coords), lat & lng
+                                 */
+                                isFocused={
+                                    lat.toPrecision(10) ===
+                                        center.lat.toPrecision(10) &&
+                                    lng.toPrecision(10) ===
+                                        center.lng.toPrecision(10)
+                                }
+                            />
+                        );
+                    })}
+                </Map>
+            </Container>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return state.Map;
+};
+
+export default connect(mapStateToProps)(Region);
---
+++ src/Containers/main.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Map from "../views/Map";
+import Marker from "../views/Marker";
+import InfoWindow from "../views/InfoWindow";
+import { mapStyle } from "../config/mapStyle";
+import { regionNames, coordinates } from "../config/regionCoords";
+const App = () => {
+    return (
+        <Map
+            defaultZoom={6}
+            apiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY}
+            defaultCenter={{ lat: 40.4710635, lng: -125.5791852 }}
+            // Map Options Props
+            mapOptions={{
+                // Zoom control options
+                zoomControl: true, // shows Zoom buttons in Google Maps
+
+                // UI Control
+                disableDefaultUI: true,
+
+                // Custom Map Style
+                styles: mapStyle
+            }}
+        >
+            {regionNames.map(region => {
+                const { lat, lng, appellations } = coordinates[region];
+
+                return (
+                    <Marker
+                        // Required Props
+                        lat={lat}
+                        lng={lng}
+                        key={region}
+                        images={{
+                            on: require(`../assets/${region}.svg`),
+                            off: require(`../assets/${region}.svg`)
+                        }}
+                        // Clickable/Info Window Props
+                        isClickable
+                        hasInfoWindow
+                        infoWindowComponent={
+                            <InfoWindow
+                                region={region}
+                                appellations={appellations}
+                            />
+                        }
+                    />
+                );
+            })}
+        </Map>
+    );
+};
+
+export default App;
