5d18a54d5a7f4d020328083bf47d72cd8c6d3a36
Victor Ragojos <vhsvragojos@gmail.com>
2019-08-08 17:27:02 -0700

    new Marker image  and reformat

--- src/views/MarkerV2.js
+++ src/views/MarkerV2.js
@@ -2,83 +2,51 @@ import React from "react";
 import { Marker } from "react-map-gl";
 
 const MarkerV2 = React.memo(props => {
-    const { lat, lng, moveToMarker } = props;
-    return (
-        <Marker anchor="bottom" latitude={lat} longitude={lng} offsetTop={-30} offsetLeft={-10}>
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="38"
-                height="44.471"
-                viewBox="0 0 38 44.471"
-                onClick={() => moveToMarker(lat, lng)}
-            >
-                <defs>
-                    <filter
-                        id="Path_2"
-                        x="0"
-                        y="0"
-                        width="38"
-                        height="44.471"
-                        filterUnits="userSpaceOnUse"
-                    >
-                        <feOffset dy="3" input="SourceAlpha" />
-                        <feGaussianBlur stdDeviation="3" result="blur" />
-                        <feFlood floodOpacity="0.231" />
-                        <feComposite operator="in" in2="blur" />
-                        <feComposite in="SourceGraphic" />
-                    </filter>
-                    <filter
-                        id="Ellipse_3"
-                        x="12"
-                        y="7.817"
-                        width="14"
-                        height="14"
-                        filterUnits="userSpaceOnUse"
-                    >
-                        <feOffset input="SourceAlpha" />
-                        <feGaussianBlur stdDeviation="1" result="blur-2" />
-                        <feFlood floodColor="#1e1e1e" floodOpacity="0.431" />
-                        <feComposite operator="in" in2="blur-2" />
-                        <feComposite in="SourceGraphic" />
-                    </filter>
-                </defs>
-                <g id="markerv2" transform="translate(5 6)">
-                    <g
-                        id="Group_2"
-                        data-name="Group 2"
-                        transform="translate(4)"
-                    >
-                        <g
-                            transform="matrix(1, 0, 0, 1, -9, -6)"
-                            filter="url(#Path_2)"
-                        >
-                            <path
-                                id="Path_2-2"
-                                data-name="Path 2"
-                                d="M14,26.471Q3.985,15.618,4,10.059a10,10,0,1,1,20,0Q24,15.618,14,26.471Z"
-                                transform="translate(5 6)"
-                                fill="#a69c80"
-                            />
-                        </g>
-                    </g>
-                    <g
-                        transform="matrix(1, 0, 0, 1, -5, -6)"
-                        filter="url(#Ellipse_3)"
-                    >
-                        <circle
-                            id="Ellipse_3-2"
-                            data-name="Ellipse 3"
-                            cx="4"
-                            cy="4"
-                            r="4"
-                            transform="translate(15 10.82)"
-                            fill="#fefdf9"
-                        />
-                    </g>
-                </g>
-            </svg>
-        </Marker>
-    );
+	const { lat, lng, moveToMarker } = props;
+	return (
+		<Marker anchor="bottom" latitude={lat} longitude={lng} offsetTop={-30} offsetLeft={-12}>
+			<svg
+				width="42"
+				height="51"
+				viewBox="0 0 42 51"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+				onClick={() => moveToMarker(lat, lng)}>
+				<g filter="url(#filter0_d)">
+					<path
+						d="M20.9992 41.4706C11.2743 30.9319 6.41906 22.9636 6.43363 17.5656C6.43363 13.7025 7.96821 9.99773 10.6998 7.26616C13.4314 4.53458 17.1362 3 20.9992 3C24.8622 3 28.567 4.53458 31.2986 7.26616C34.0302 9.99773 35.5648 13.7025 35.5648 17.5656C35.5648 22.9636 30.7096 30.9319 20.9992 41.4706Z"
+						fill="#A69C80"
+					/>
+				</g>
+				<path
+					d="M21.0001 21.587C24.2178 21.587 26.8263 18.9785 26.8263 15.7608C26.8263 12.5431 24.2178 9.93457 21.0001 9.93457C17.7823 9.93457 15.1738 12.5431 15.1738 15.7608C15.1738 18.9785 17.7823 21.587 21.0001 21.587Z"
+					fill="#FEFDF9"
+				/>
+				<defs>
+					<filter
+						id="filter0_d"
+						x="0.433594"
+						y="0"
+						width="41.1312"
+						height="50.4706"
+						filterUnits="userSpaceOnUse"
+						colorInterpolationFilters="sRGB">
+						<feFlood floodOpacity="0" result="BackgroundImageFix" />
+						<feColorMatrix
+							in="SourceAlpha"
+							type="matrix"
+							values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+						/>
+						<feOffset dy="3" />
+						<feGaussianBlur stdDeviation="3" />
+						<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.231 0" />
+						<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow" />
+						<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape" />
+					</filter>
+				</defs>
+			</svg>
+		</Marker>
+	);
 });
 
 export default MarkerV2;
