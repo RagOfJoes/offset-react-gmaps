767a391e54d59191791649835d8fdcc812cb070f
Victor Ragojos <vhsvragojos@gmail.com>
2019-08-15 17:15:20 -0700

    removed unused routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import "./App.scss";
 import React from "react";
 import MapV2 from "./Containers/v2";
 import main from "./Containers/main";
-import { connect } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import CentralCoast from "./Containers/central-coast";
-import WillametteValley from "./Containers/willamette-valley";
-import NorthernCalifornia from "./Containers/northern-california";
 
 class App extends React.Component {
 	render() {
@@ -14,18 +10,11 @@ class App extends React.Component {
 			<div className="App">
 				<Switch>
 					<Route exact path="/" component={main} />
-					<Route path="/willamette-valley" component={WillametteValley} />
-					<Route path="/northern-california" component={NorthernCalifornia} />
-					<Route path="/central-coast" component={CentralCoast} />
-					<Route path="/v2" component={MapV2} />
+					<Route path="/maps" component={MapV2} />
 				</Switch>
 			</div>
 		);
 	}
 }
 
-const mapStateToProps = state => {
-	return state.Map;
-};
-
-export default connect(mapStateToProps)(App);
+export default App;
