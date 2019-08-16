8bd21dd9e2a2dddc72fef3bdcf546c025ae6a179
Victor Ragojos <vhsvragojos@gmail.com>
2019-08-15 17:15:11 -0700

    removed unused Redux context

--- src/Redux/Reducers/root.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import mapReducer from '../Reducers/Map';
-
-export const root = combineReducers({
-    Map: mapReducer
-})
--- src/Redux/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux';
-import { root } from './Reducers/root';
-
-export const store = createStore(
-    root
-);
