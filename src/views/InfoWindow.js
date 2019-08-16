53bb9cf7ad4098f4a47da06051635b4c47ddc9e3
Victor Ragojos <vhsvragojos@gmail.com>
2019-08-16 10:47:16 -0700

    small sytactical changes

--- src/views/InfoWindow.js
+++ src/views/InfoWindow.js
@@ -1,39 +1,38 @@
 import React from "react";
 
 /**
- * Custom Info Window content for Google Maps
+ * InfoWindow Component
  * 
- * @property {string} region
- * @property {string} appellations
+ * @param {object} props Required Props for View Component 
  * 
- * @version 1.0.0
- * @author [Victor Ragojos](https://github.com/ragofjoes)
+ * @version 2.0.0
+ * @author [Victor Ragojos](https://github.com/RagOfJoes)
  */
-const InfoWindow = React.memo(props => {
-    const { region, appellations } = props;
+const InfoWindow = props => {
+	const { image, region, description } = props;
 
-    // Will essentially replace all spaces with - to make URL for SEO friendly
-    const regionLink = region.split(' ').join('-').toLowerCase();
+	// Will essentially replace all spaces with - to make URL for SEO friendly
+	const regionLink = region
+		.split(" ")
+		.join("-")
+		.toLowerCase();
 
-    return (
-        <div className="info-window-container">
-            <div className="info-window-image">
-                <img
-                    src={require("../assets/temp-vineyard-img.png")}
-                    alt="Region"
-                />
-            </div>
-            <div className="info-window-information">
-                <h2 className="info-window-region">{region}</h2>
-                <p className="info-window-appellations">
-                    <strong>APPELLATIONS: </strong>
-                    {appellations}
-                </p>
-                <p className="info-window-link">
-                <a href={`https://staging-dawecude.kinsta.cloud/vineyards/${regionLink}`} target="_top">Explore Map</a>                </p>
-            </div>
-        </div>
-    );
-});
+	return (
+		<div className="popup-container">
+			<div className="popup-image">
+				<img alt="Region" src={image} />
+			</div>
+			<div className="info-window-information">
+				<p className="info-window-region">{region}</p>
+				<p className="info-window-appellations">{description}</p>
+				<p className="info-window-link">
+					<a href={`https://staging-dawecude.kinsta.cloud/vineyards/${regionLink}`} target="_top">
+						Explore Vineyard
+					</a>{" "}
+				</p>
+			</div>
+		</div>
+	);
+};
 
 export default InfoWindow;
