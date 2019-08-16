237b7d6ae0adb311ba5f103fa513e008334e4b24
Victor Ragojos <vhsvragojos@gmail.com>
2019-08-15 17:18:57 -0700

    added custom Image props

--- src/views/InfoWindowV2.js
+++ src/views/InfoWindowV2.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const InfoWindow = props => {
-	const { region, description } = props;
+	const { image, region, description } = props;
 
 	// Will essentially replace all spaces with - to make URL for SEO friendly
 	const regionLink = region
@@ -12,10 +12,10 @@ const InfoWindow = props => {
 	return (
 		<div className="popup-container">
 			<div className="popup-image">
-				<img alt="Region" src={require("../assets/temp-vineyard-img.png")} />
+				<img alt="Region" src={image} />
 			</div>
 			<div className="info-window-information">
-				<h2 className="info-window-region">{region}:</h2>
+				<p className="info-window-region">{region}</p>
 				<p className="info-window-appellations">{description}</p>
 				<p className="info-window-link">
 					<a href={`https://staging-dawecude.kinsta.cloud/vineyards/${regionLink}`} target="_top">
