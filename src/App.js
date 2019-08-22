dfa99b2ffc12a5869d3bab7b9c05f797702077cb
Victor Ragojos <vhsvragojos@gmail.com>
2019-08-21 22:16:01 -0700

    seperated different regions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import "./App.scss";
-import React from "react";
-import maps from "./Containers/maps";
-import main from "./Containers/main";
-import { Route, Switch } from "react-router-dom";
+import './App.scss';
+import React from 'react';
+import Maps from './Containers/maps';
+import OverviewMap from './Containers/main';
+import { sections } from './config/regions';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends React.Component {
 	render() {
 		return (
 			<div className="App">
 				<Switch>
-					<Route exact path="/" component={main} />
-					<Route exact path="/maps" component={maps} />
+					<Route exact path="/" component={OverviewMap} />
+					<Route
+						exact
+						path="/:region"
+						render={props => {
+							return <Maps region={sections[props.match.params.region]} />;
+						}}
+					/>
 				</Switch>
 			</div>
 		);
