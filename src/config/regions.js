d22c4f7febcff11637f5c4c5eb47b61151d9a09e
RagOfJoes <apps@ragofjoes.dev>
2022-08-09 17:44:31 -0700

    chore: add Hirsch Vineyard and Sonoma Valley

--- src/config/regions.js
+++ src/config/regions.js
@@ -8,31 +8,36 @@
  * @author [Victor Ragojos](https://github.com/RagOfJoes)
  */
 export const sections = {
-	'willamette-valley': {
-		sectionTitle: 'Willamette Valley',
-		appellations: 'Willamette Valley, Dundee Hills',
-		description: `Oregon’s largest appellation, the Willamette Valley sits between the Oregon Coast Range in the west and the Cascade Mountains in the east.`,
-		vineyards: ['Prince Hill Vineyard']
-	},
-	'north-coast': {
-		sectionTitle: 'North Coast',
-		appellations: 'Anderson Valley, Russian River Valley, Dry Creek Valley, Napa Valley, Oakville, Calistoga',
-		description: `Home to renowned California growing regions also known as Wine County, Northern California has deep roots in viticulture and winemaking.`,
-		vineyards: [
-			'Monument Tree Vineyard',
-			'Bearman Bend Vineyard',
-			'Healdsburg Estate Vineyard',
-			'Westpin Vineyard',
-			'Last Stop',
-			'Merino Vineyard',
-			'Calistoga Estate Vineyard',
-			'Oakville Estate Vineyard',
-		]
-	},
-	'central-coast': {
-		sectionTitle: 'Central Coast',
-		appellations: 'Santa Maria Valley, Santa Lucia Highlands',
-		vineyards: ["Garys' Vineyard", 'Soberanes Vineyard', 'Bien Nacido Vineyard'],
-		description: `Comprised of six counties from Ventura (north of Los Angeles) to Santa Cruz (south of San Francisco), Central Coast spans much of California’s Pacific coastline.`
-	}
+  "willamette-valley": {
+    sectionTitle: "Willamette Valley",
+    appellations: "Willamette Valley, Dundee Hills",
+    description:
+      "Oregon’s largest appellation, the Willamette Valley sits between the Oregon Coast Range in the west and the Cascade Mountains in the east.",
+    vineyards: ["Prince Hill Vineyard"],
+  },
+  "north-coast": {
+    sectionTitle: "North Coast",
+    appellations:
+      "Anderson Valley, Sonoma Valley, Russian River Valley, Dry Creek Valley, Napa Valley, Oakville, Calistoga",
+    description:
+      "Home to renowned California growing regions also known as Wine County, Northern California has deep roots in viticulture and winemaking.",
+    vineyards: [
+      "Monument Tree Vineyard",
+      "Bearman Bend Vineyard",
+      "Hirsch Vineyard",
+      "Healdsburg Estate Vineyard",
+      "Westpin Vineyard",
+      "Last Stop",
+      "Merino Vineyard",
+      "Calistoga Estate Vineyard",
+      "Oakville Estate Vineyard",
+    ],
+  },
+  "central-coast": {
+    sectionTitle: "Central Coast",
+    appellations: "Santa Maria Valley, Santa Lucia Highlands",
+    description:
+      "Comprised of six counties from Ventura (north of Los Angeles) to Santa Cruz (south of San Francisco), Central Coast spans much of California’s Pacific coastline.",
+    vineyards: ["Garys’ Vineyard", "Soberanes Vineyard", "Bien Nacido Vineyard"],
+  },
 };
