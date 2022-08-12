9cf500e63f4618d3b6985888ff0d9e8f37bbb1c9
RagOfJoes <apps@ragofjoes.dev>
2022-08-12 12:29:06 -0700

    fix: build output dir for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build',
+  },
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
