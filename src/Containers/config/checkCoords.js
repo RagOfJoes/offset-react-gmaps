ca70f68247292e735b4796e642c952796ae8043e
RagOfJoes <apps@ragofjoes.dev>
2022-08-12 12:06:53 -0700

    chore: remove old files

--- src/Containers/config/checkCoords.js
+++
@@ -1,5 +0,0 @@
-import { toPrecise } from '../../views/Card/config';
-
-export const checkCoord = (startLat, startLng, endLat, endLng) => {
-	return toPrecise(startLat) === toPrecise(endLat) && toPrecise(startLng) === toPrecise(endLng);
-};
